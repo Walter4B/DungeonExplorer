2ba1d7eb4feaee0f6ec072855d5699e9f33db57c
Walter_Ba <walter.bali@outlook.com>
2019-02-25 19:52:28 +0100

    Added Player + Player Movement + Camera Controller

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 2;
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        transform.position = transform.position + (movement * Speed) * Time.deltaTime;
+    }
+}
+
+    
