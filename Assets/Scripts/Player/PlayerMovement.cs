073bb9eec775da0cb29f8836a6d148d46e7eb9e2
Walter_Ba <walter.bali@outlook.com>
2019-02-26 16:47:55 +0100

    Colliders on walls and player. Player Z rotation locked. Player GravityScale set to 0. Added mouse CrossHair.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 2;
+    public GameObject crossHair;
 
     void Update()
+    {
+        Movement();
+        MoveCrossHair();
+    }
+
+    public void Movement()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
         transform.position = transform.position + (movement * Speed) * Time.deltaTime;
+        MoveCrossHair();
+    }
+
+    private void MoveCrossHair()
+    {
+        Cursor.visible = false;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x, mousePosition.y);
+
+        crossHair.transform.position = direction;
+
     }
 }
 
