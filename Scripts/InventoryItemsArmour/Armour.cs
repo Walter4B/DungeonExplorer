40a6d481ac4669c056196cb3bbb419b3e7bf31a3
krompac <tomijehrvat@gmail.com>
2019-04-22 16:58:59 +0200

    test komitanja

---
+++ Scripts/InventoryItemsArmour/Armour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Armour
+{
+    public enum PossibleArmourSlots { Head, Shoulders, Chest, Hands, Legs, Boots}
+    public enum ArmourType { Cloth, Light, Medium, Heavy}
+    public enum Rarity { Common, Rare, Epic, Legendary}
+
+    public static int GetArmourValue(PossibleArmourSlots slot)
+    {
+        switch (slot)
+        {
+            case PossibleArmourSlots.Head:
+                return 5;
+            case PossibleArmourSlots.Shoulders:
+                return 6;
+            case PossibleArmourSlots.Chest:
+                return 20;
+            case PossibleArmourSlots.Hands:
+                return 5;
+            case PossibleArmourSlots.Legs:
+                return 10;
+            case PossibleArmourSlots.Boots:
+                return 5;
+        }
+
+        return 0;
+    }
+
+    public static int GetSlotIndex(PossibleArmourSlots slot)
+    {
+        var slots = Enum.GetNames(typeof(PossibleArmourSlots)).ToList();
+        return slots.IndexOf(slot.ToString());
+    }
+
+    public static PossibleArmourSlots GetArmour()
+    {
+        var slotList = Enum.GetNames(typeof(PossibleArmourSlots)).ToList();
+        slotList.Reverse();
+
+        return (PossibleArmourSlots)Enum.Parse(typeof(PossibleArmourSlots), slotList[RandomNumberGenerator.GetRandom(6)]);
+    }
+}
---
+++ Scripts/InventoryItemsArmour/PanelValues.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelValues : MonoBehaviour
+{
+    public Image image;
+    new public Text name;
+    public Text armourValue;
+    public Text staminaValue;
+    public Text strenghtValue;
+    public Text IntellectValue;
+
+    public void UpdateValues(ArmourPiece item)
+    {
+        if (item != null)
+        {
+            var values = item.Values();
+            name.text = values[0];
+            armourValue.text = values[1];
+            staminaValue.text = values[2];
+            strenghtValue.text = values[3];
+            IntellectValue.text = values[4];
+
+            if (item.icon != null)
+            {
+                image.sprite = item.icon;
+            }
+        }
+    }
+}
